b32be63ad8c515cde0faa6d64a0334281290647f
emmanuel cabral <emmanuel_cabral@hotmail.com>
2015-12-09 11:47:09 -0300

    Se agrego el servicio para el envio de mail.

---
+++ Services/Mailer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tecspro\Bundle\ComunBundle\Services;
+
+class Mailer {
+
+    public function send_mailer($correos, $mensaje) {
+
+        if (!empty($correos)) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject("Mensaje del sistema de alertas")
+                ->setFrom('siaval@gmail.com')
+                ->setTo($correos)
+                ->setBody($mensaje)
+                )
+            ;
+            $this->get('mailer')->send($message);
+        }
+        return null;
+    }
+}
+
+
