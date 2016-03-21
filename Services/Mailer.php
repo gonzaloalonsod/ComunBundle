d9d77110d6332868474ca81acb70537aa602cf7b
Gonzalo Alonso <gonkpo@gmail.com>
2016-03-21 20:28:39 -0300

    fix envio cco

--- Services/Mailer.php
+++ Services/Mailer.php
@@ -3,27 +3,24 @@
 namespace Tecspro\Bundle\ComunBundle\Services;
 
 class Mailer {
-
     protected $mailer;
 
-    public function __construct($mailer){
-        
+    public function __construct($mailer)
+    {
         $this->mailer = $mailer;
     }
     
-    public function send_mailer($asunto, $remitente, $correos, $mensaje) {
-
+    public function send_mailer($asunto, $remitente, $correos, $mensaje)
+    {
         if (!empty($correos)) {
-
             $message = \Swift_Message::newInstance()
                 ->setSubject($asunto)
                 ->setFrom($remitente)
-                ->setTo($correos)
+                ->setTo($remitente)
+                ->setBcc($correos)
                 ->setBody($mensaje);   
             ;
             $this->mailer->send($message);
         }       
     }
 }
-
-
