d8f3e49dc704bacbbace8acdc31886970fb7e985
Gonzalo <gonkpo@gmail.com>
2016-05-19 11:50:01 -0300

    control js funcionando.

--- Resources/public/js/controla_patron.js
+++ Resources/public/js/controla_patron.js
@@ -18,8 +18,8 @@ function validaPatron(event, patron) {
 returnGlobal = false;
 // EJECUTA PRIMERO
 $(".mwsValidaPatron").on("keydown", function(event) {
+    returnGlobal = false;
     tecla = event.which;
-    console.log(tecla);
     // Teclas de retroceso para borrar(8), tab(9), enter(13), flecha izquierda(37), derecha(39),
     // suprimir(46), siempre la permite
     if (tecla==8 || tecla==9 || tecla==13 || tecla==37 || tecla==39 || tecla==46) {
@@ -37,12 +37,10 @@ $(".mwsValidaPatron").on("keypress", function(event) {
                 patron = attr.value;
             }
         });
-        console.log(patron);
         patron = new RegExp(patron);
 
         tecla       = (document.all) ? event.keyCode : event.which;
         tecla_final = String.fromCharCode(tecla);
-        console.log(tecla_final);
 
         return patron.test(tecla_final);
     }
